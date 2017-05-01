687e453d1f0c93503023e20ab16cb74e12c7cc4a
Александр Шинкевич <a.shinkevich1998@gmail.com>
2017-05-01 23:03:04 +0300

    Исправлен расчёт измеренных значений

--- Source.cpp
+++ Source.cpp
@@ -26,7 +26,7 @@ int main() {
 	cout << "\nVvedite kolichestvo preobrazovanii l: ";
 	cin >> l;
 	cout << "\nVvedite matricu koefficientov A: \n";
-	for (j = 0; j <= k; j++) {
+	for (j = 0; j < k + 1; j++) {
 		for (i = 0; i < l; i++) {
 			cin >> A[j][i];
 		}
@@ -39,8 +39,8 @@ int main() {
 		}
 		for (i = 0; i < l; i++) {
 			Z[i] = A[0][i];
-			for (j = 1; j < k; j++) {
-				Z[i] = Z[i] + A[j][i] * X[j];
+			for (j = 1; j < k + 1; j++) {
+				Z[i] = Z[i] + A[j][i] * X[j-1];
 			}
 			fout << Z[i] << ' ';
 		}
@@ -68,12 +68,13 @@ int main() {
 	}
 	fin.close();
 	C = 0;
-	cout << 'N';
+	cout << 'N' << "     ";
 	for (i = 0; i < k; i++) {
-		cout << fixed << "      X[" << i + 1 << "]";
+		cout << fixed << ' ' << "X[" << i + 1 << "]" << '\t';
 	}
+	cout << '\t';
 	for (j = 0; j < l; j++) {
-		cout << fixed << "         Z[" << j + 1 << "]";
+		cout << fixed << ' ' << "Z[" << j + 1 << "]" << '\t';
 	}
 	cout << '\n';
 	while (C < N) {
@@ -83,7 +84,7 @@ int main() {
 		}
 		cout << ' ';
 		for (i = 0; i < l; i++) {
-			cout << fixed << ' ' << Z[i];
+			cout << fixed << ' ' << Z_data[i][C];
 		}
 		Sleep(T * 1000);
 		C++;
